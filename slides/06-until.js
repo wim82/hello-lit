2cd94dec8e7cc029b09f0daff0921783fc1f25dd
wim maesschalck <wim.maesschalck@ing.com>
2018-11-26 09:41:31 +0100

    small changes to the until demo

--- slides/06-until.js
+++ slides/06-until.js
@@ -1,11 +1,12 @@
 import { html, render } from 'https://unpkg.com/lit-html@0.12.0/lit-html.js';
 import { until } from  'https://unpkg.com/lit-html@0.12.0/directives/until.js';
 
-const somethingHappens = () => new Promise((resolve) => setTimeout(() => resolve('something'), 2000));
+const somethingHappens = () => new Promise((resolve) => setTimeout(() => resolve('an incredible update'), 4000));
 
 render(html`
-        ${until(
-          somethingHappens().then(something => html`${something} happened!`),
-          html`<span>loading ...</span>`)
+        <h3>until() directive</h3>
+        ${until(somethingHappens()
+                .then(something => html`${something} happened!`),
+                html`<span>loading ...</span>`)
         }
       `, document.querySelector('#first'));
